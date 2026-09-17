80a8ec46c53b367c565c7ba6053dae59da02ca06
vlogsrk242-cyber <vlogsrk242@gmail.com>
2026-09-17 13:15:49 +0530

    Update main.dart

--- sheth_app/lib/main.dart
+++ sheth_app/lib/main.dart
@@ -1,6 +1,8 @@
 import 'package:flutter/material.dart';
 import 'login_page.dart';
+
 void main() {
+  WidgetsFlutterBinding.ensureInitialized();
   runApp(const HiraWorkShethApp());
 }
 
@@ -17,67 +19,6 @@ class HiraWorkShethApp extends StatelessWidget {
         colorSchemeSeed: Colors.blue,
       ),
       home: const LoginPage(),
-    );
-  }
-}
-
-class HomePage extends StatefulWidget {
-  const HomePage({super.key});
-
-  @override
-  State<HomePage> createState() => _HomePageState();
-}
-
-class _HomePageState extends State<HomePage> {
-  int currentIndex = 0;
-
-  final List<Widget> pages = const [
-    Center(child: Text('તળીયા')),
-    Center(child: Text('પેલ')),
-    Center(child: Text('મથાળા')),
-    Center(child: Text('કારીગર')),
-  ];
-
-  @override
-  Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(
-        title: const Text('હીરા કામ હિસ્ટરી'),
-        centerTitle: true,
-      ),
-
-      body: pages[currentIndex],
-
-      bottomNavigationBar: NavigationBar(
-        selectedIndex: currentIndex,
-        onDestinationSelected: (index) {
-          setState(() {
-            currentIndex = index;
-          });
-        },
-        destinations: const [
-          NavigationDestination(
-            icon: Icon(Icons.diamond_outlined),
-            selectedIcon: Icon(Icons.diamond),
-            label: 'તળીયા',
-          ),
-          NavigationDestination(
-            icon: Icon(Icons.work_outline),
-            selectedIcon: Icon(Icons.work),
-            label: 'પેલ',
-          ),
-          NavigationDestination(
-            icon: Icon(Icons.layers_outlined),
-            selectedIcon: Icon(Icons.layers),
-            label: 'મથાળા',
-          ),
-          NavigationDestination(
-            icon: Icon(Icons.people_outline),
-            selectedIcon: Icon(Icons.people),
-            label: 'કારીગર',
-          ),
-        ],
-      ),
     );
   }
 }
